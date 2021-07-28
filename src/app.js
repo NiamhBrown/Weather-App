e5c06a8c49204621b53fd484500980da7d033430
NiamhBrown <85416454+NiamhBrown@users.noreply.github.com>
2021-07-28 22:49:08 +0100

    raplaced fake data with data from API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
+
   let temperatureElement = document.querySelector("#main-temp-value");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 let currentCity = "Brighton"; //`${searchInput.value}`;
 let units = "metric";
 let apiKey = "e4c991b27b566dc4b5b311b6f8d9ac5c";
