e62497a5fed4e3d4b1f80fcf9930a1fccb2b2bcd
NiamhBrown <85416454+NiamhBrown@users.noreply.github.com>
2021-07-29 10:38:56 +0100

    added active class to links

--- src/app.js
+++ src/app.js
@@ -68,12 +68,16 @@ function displayFahrenheitTemperature(event) {
   let temperatureElement = document.querySelector("#main-temp-value");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#main-temp-value");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
 }
 let celsiusTemperature = null;
 
