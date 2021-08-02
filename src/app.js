4f3ee77057f8109e1dcb24d5be6507ee1a56678d
NiamhBrown <85416454+NiamhBrown@users.noreply.github.com>
2021-08-02 14:24:02 +0100

    removed imperial conversion

--- src/app.js
+++ src/app.js
@@ -35,8 +35,7 @@ function formatDay(timestamp) {
   return days[day];
 }
 
- //function displayAffirmation() {
-  
+//function displayAffirmation() {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
@@ -112,32 +111,8 @@ function handleSubmit(event) {
   search(searchInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#main-temp-value");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#main-temp-value");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-}
-let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Brighton");
 
